9c93390f2e3cc047be6cefdc69469f244fefb06c
erwinfattah <68496276+erwinfattah@users.noreply.github.com>
2021-03-31 22:26:41 +0700

    Delete Player.java and Edit PlayerRound.java

--- PlayerRound.java
+++ PlayerRound.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PlayerRound {
     private Player [] players;
+
+
+    PlayerRound(int jumlahPemain){
+    Scanner scan = new Scanner(System.in);
+    for (int i = 0;i<(jumlahPemain-1);i++)
+    {
+        System.out.print("Masukkan nama pemain : ");
+        players[i]=scan.nextLine();
+    }
+    }
 
     
     
