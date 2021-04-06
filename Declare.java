becbeb7e382e3264c64dfc4e1be3c9ea4377bb91
azkaalya <azkaalya8@gmail.com>
2021-04-06 23:42:05 +0700

    update declare

--- Declare.java
+++ Declare.java
@@ -1,31 +1,27 @@
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class Declare {
-    static volatile boolean isTimeCompleted = true;
-
-    // public static void main(String[] args) {
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-
-            @Override
-            public void run() {
-                if (isTimeCompleted) {
-                    System.out.println("Waktu kamu habis, kamu akan mendapat tambahan 2 kartu!");
-                    
-                }
-            }
-        };
-        timer.schedule(task, 3000);
-
-        System.out.println("Ketik HIJI dalam 3 detik!");
-
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        isTimeCompleted = false;
-
-
-        System.out.println("Berhasil!");
-    // }
+boolean pid_hiji = false;
+if (getPlayerHandSize(playerIds[i] == 1){
+    pid_hiji = true;
+}
+while (pid_hiji){
+    System.out.println("Waktunya declare HIJI!");
+    long startTime;
+    String input_hiji;
+    boolean loop;
+    startTime = System.currentTimeMillis();
+    while (loop){
+        System.out.println("Ketik HIJI dalam 3 detik");
+        inputhiji = input.next().toUpperCase();
+    }
+    if (inputhiji.equals("HIJI")){
+        loop = false;
+        pid_hiji = false;
+        System.out.println("Declare HIJI sukses!");
+    }
+if (System.currentTimeMillis() - startTime > 3000){
+    System.out.println("Kamu telat declare HIJI!");
+    getPlayerHand(pid).add(deck.drawCard());
+    getPlayerHand(pid).add(deck.drawCard());
+    player_hiji = false;
+}
 }
